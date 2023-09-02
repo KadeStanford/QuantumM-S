addc1af7ef32134d4973741128ec6f8a0855a929
kade <kade20413@gmail.com>
2023-09-02 01:21:35 -0500

    yes

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Exclude 'fs', 'net', and 'dns' from being bundled in the client-side build
-      config.node = {
-        fs: "empty",
-        net: "empty",
-        dns: "empty",
-      };
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
